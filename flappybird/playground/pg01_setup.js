3e2f8689a60440909962cdd88540dd33cb43e5fa
wooof4 <tytus8392@gmail.com>
2026-07-26 04:31:57 +0000

    Jul 26, 2026, 4:31 AM

---
+++ flappybird/playground/pg01_setup.js
@@ -0,0 +1,8 @@
+let bird,floor;
+let flapMidImg,bg,base
+
+function preload(){
+    flapMidImg=loadimage('assets/redbird-midflap.png')
+    bg=loadimage('assets/backround-night.png')
+    base=loadimage('assets/base.png')
+}
