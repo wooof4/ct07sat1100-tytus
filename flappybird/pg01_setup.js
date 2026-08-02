4f1a38d80e37f0a37fe1cb53cd946ae85d409851
wooof4 <tytus8392@gmail.com>
2026-08-02 03:51:03 +0000

    Aug 2, 2026, 3:51 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -19,7 +19,7 @@ function setup(){
     bird.height=100;
     bird.img=flapMidImg;
     bird.collider = 'dynamic';
-    bird.
+    bird.drag=-100;
     bird.mass=2;
     bird.bounciness=0.5;
     world.gravity.y=-10;
