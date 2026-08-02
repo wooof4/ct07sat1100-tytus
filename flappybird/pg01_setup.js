335c6b762fcdee2e89c425bc74822f1064458ed7
wooof4 <tytus8392@gmail.com>
2026-08-02 04:15:05 +0000

    Aug 2, 2026, 4:15 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -61,6 +61,6 @@ function draw(){
     
     FileList('blue');
     textSize(14);
-    text('vel.y'+bird.vel.y.tofixed(2),10,20);
-    text('isss')
+    text('vel.y:'+bird.vel.y.tofixed(2),10,20);
+    text('isMoving:'+bird.isM)
 }
