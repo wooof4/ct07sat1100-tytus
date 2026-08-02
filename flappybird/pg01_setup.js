2a1d0ddaa2830ad2c7662d49cdf8f4b31b35fdbe
wooof4 <tytus8392@gmail.com>
2026-08-02 03:40:06 +0000

    Aug 2, 2026, 3:40 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -2,9 +2,9 @@ let bird,floor;
 let flapMidImg,bg,base;
 
 function preload(){
-    flapMidImg=loadimage('assets/redbird-midflap.png');
-    bg=loadimage('assets/background-night.png');
-    base=loadimage('assets/base.png');
+    flapMidImg=loadImage('assets/redbird-midflap.png');
+    bg=loadImage('assets/background-night.png');
+    base=loadImage('assets/base.png');
 }
 
 function setup(){
