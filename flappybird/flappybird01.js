26b81ec96d92dcc581ed2b49b9609da046c71bc1
wooof4 <tytus8392@gmail.com>
2026-08-02 04:22:49 +0000

    Aug 2, 2026, 4:22 AM

--- flappybird/flappybird01.js
+++ flappybird/flappybird01.js
@@ -1,18 +1,81 @@
-// the final game
 let bird,floor;
-let flapMidImg,bg,base
+let flapMidImg,bg,base;
 
 function preload(){
-    flapMidImg=loadimage('assets/redbird-midflap.png')
-    bg=loadimage('assets/backround-night.png')
-    base=loadimage('assets/base.png')
+    flapMidImg=loadImage('assets/redbird-midflap.png');
+    bg=loadImage('assets/background-night.png');
+    base=loadImage('assets/base.png');
 }
 
 function setup(){
     createCanvas(400,600);
-    background('white')
+    background('white');
+
+    // //sprite
+    bird=new Sprite();
+    bird.x=width/2;
+    bird.y=200;
+    bird.width=10;
+    bird.height=10;
+    bird.img=flapMidImg;
+    bird.collider = 'dynamic';
+    // bird.drag=-100;
+    bird.mass=2;
+    bird.bounciness=0.5;
+    world.gravity.y=20;
+    // world.gravity.x=20;
+    floor = new Sprite()
+    floor.x=200;
+    floor.y=height -20;
+    floor.width=400;
+    floor.height=125;
+    floor.collider='static'
+    floor.img=base;
+    wall1= new Sprite()
+    wall1.x=420;
+    wall1.y=300;
+    wall1.width=20;
+    wall1.height=600;
+    wall1.collider='static';
+    // wall2=new Sprite()
+    // wall2.x=-20;
+    // wall2.y=300;
+    // wall2.width=20;
+    // wall2.height=600;
+    // wall2.collider='static';
+    // ceiling=new Sprite()
+    // ceiling.x=200
+    // ceiling.y=-20
+    // ceiling.width=400
+    // ceiling.height=20
+    // ceiling.collider='static'
 }
 
-function draw(){
 
+function draw(){
+    image(bg,0,0,width,height);
+    if (kb.presses('space')){
+        bird.vel.y=-15
+        bird.sleeping=false
+    }
+
+//     if(kb.presses('d')){
+//         bird.vel.x=15
+//         bird.sleeping=false
+// }
+//     if(kb.presses('a')){
+//         bird.vel.x=-15
+//         bird.sleeping=false
+// }
+
+//     if(kb.presses('s')){
+//         bird.vel.y=15
+//         bird.sleeping=false
+// }
+    
+    fill('blue');
+    textSize(14);
+    text('vel.y:'+bird.vel.y.toFixed(2),10,20);
+    text('isMoving:'+bird.isMoving,10,40);
+    text('sleeping:'+bird.sleeping,10,60);
 }
