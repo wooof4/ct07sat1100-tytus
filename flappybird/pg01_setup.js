d962537f7af0e6a77f9fb569d60d9ee4984293c3
wooof4 <tytus8392@gmail.com>
2026-08-23 04:20:50 +0000

    Aug 23, 2026, 4:20 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -88,7 +88,8 @@ if (bird.collides(pipeGroup)||bird.collides(floor)){
     gameoverLabel=new Sprite(width/2,height/2,192,42);
     gameoverLabel.img=gameoverImg;
     gameoverLabel.layer=100;
-    gameoverLabel.x
+    gameoverLabel.x=camera.x;
+    
     noLoop();
 }
 }
