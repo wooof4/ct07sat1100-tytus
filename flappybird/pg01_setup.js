e4c93615879703a68f83cdab10aae4daba40eb1e
wooof4 <tytus8392@gmail.com>
2026-08-23 04:26:04 +0000

    Aug 23, 2026, 4:26 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -13,7 +13,7 @@ bg = loadImage('assets/background-night.png');
 base = loadImage('assets/base.png');
 pipe = loadImage('assets/pipe-green.png');
 gameoverImg=loadImage('assets/gameover.png');
-startScreenimg=loadImage
+startScreenimg=loadImage('assets/message')
 }
 
 function setup(){
