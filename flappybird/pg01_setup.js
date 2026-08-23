be43553b55a458de36bca6775a5b7bde81b8282a
wooof4 <tytus8392@gmail.com>
2026-08-23 04:27:32 +0000

    Aug 23, 2026, 4:27 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -43,7 +43,7 @@ floor.img = base;
 pipeGroup = new Group();
 
 startScreenLabel=new Sprite(width/2,height/2,50,50,'none')
-startScreenLabel
+startScreenLabel.img=startScreenimg;
 }
 
 function draw(){
