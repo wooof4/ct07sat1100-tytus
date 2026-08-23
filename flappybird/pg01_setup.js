d52d1e29a7a8bcb4f1f3a13c4fd97f193c3bb8a4
wooof4 <tytus8392@gmail.com>
2026-08-23 04:01:04 +0000

    Aug 23, 2026, 4:01 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -88,7 +88,7 @@ function spawnPipePair(){
 let gap = 50;
 let midY = height / 2;
 
-bottomPipe = new Sprite(bird.x+0, midY + gap / 2 + 200, 52, 320, 'static');
+bottomPipe = new Sprite(bird.x+400, midY + gap / 2 + 200, 52, 320, 'static');
 bottomPipe.img = pipe;
 
 pipeGroup.add(bottomPipe);
