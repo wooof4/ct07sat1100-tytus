8d602a0d18c8effe0509761bc01dc5066ab67af8
wooof4 <tytus8392@gmail.com>
2026-08-23 03:37:08 +0000

    Aug 23, 2026, 3:37 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -1,81 +1,91 @@
-let bird,floor;
-let flapMidImg,bg,base;
+let bird, floor;
+let flapMidImg, bg, base;
+let pipeGroup, pipe, bottomPipe, topPipe;
 
-function preload(){
-    flapMidImg=loadImage('assets/redbird-midflap.png');
-    bg=loadImage('assets/background-night.png');
-    base=loadImage('assets/base.png');
+function preload() {
+flapMidImg = loadImage('assets/yellowbird-midflap.png');
+flapUpImg = loadImage('assets/yellowbird-upflap.png');
+flapDownImg = loadImage('assets/yellowbird-downflap.png');
+bg = loadImage('assets/background-night.png');
+base = loadImage('assets/base.png');
+pipe = loadImage('assets/pipe-green.png');
+
 }
 
 function setup(){
-    createCanvas(400,600);
-    background('white');
+new Canvas(400,600);
 
-    // //sprite
-    bird=new Sprite();
-    bird.x=width/2;
-    bird.y=200;
-    bird.width=10;
-    bird.height=10;
-    bird.img=flapMidImg;
-    bird.collider = 'dynamic';
-    // bird.drag=-100;
-    bird.mass=2;
-    bird.bounciness=0.5;
-    world.gravity.y=20;
-    // world.gravity.x=20;
-    floor = new Sprite()
-    floor.x=200;
-    floor.y=height -20;
-    floor.width=400;
-    floor.height=125;
-    floor.collider='static'
-    floor.img=base;
-    wall1= new Sprite()
-    wall1.x=420;
-    wall1.y=300;
-    wall1.width=20;
-    wall1.height=600;
-    wall1.collider='static';
-    // wall2=new Sprite()
-    // wall2.x=-20;
-    // wall2.y=300;
-    // wall2.width=20;
-    // wall2.height=600;
-    // wall2.collider='static';
-    // ceiling=new Sprite()
-    // ceiling.x=200
-    // ceiling.y=-20
-    // ceiling.width=400
-    // ceiling.height=20
-    // ceiling.collider='static'
+bird = new Sprite();
+bird.x = width / 2;
+bird.y = 200;
+bird.width = 30;
+bird.height = 30;
+bird.img = flapMidImg;
+
+bird.collider = "dynamic";
+bird.mass = 2;
+bird.drag = 0.02;
+bird.bounciness = 0.5;
+world.gravity.y = 10;
+
+floor = new Sprite();
+floor.x = 200;
+floor.y = height - 20;
+floor.width = 400;
+floor.height = 125;
+floor.collider = "static";
+floor.img = base;
+
+pipeGroup = new Group();
 }
 
-
 function draw(){
-    image(bg,0,0,width,height);
-    if (kb.presses('space')){
-        bird.vel.y=-15
-        bird.sleeping=false
-    }
+image(bg,0,0,width,height);
 
-//     if(kb.presses('d')){
-//         bird.vel.x=15
-//         bird.sleeping=false
-// }
-//     if(kb.presses('a')){
-//         bird.vel.x=-15
-//         bird.sleeping=false
-// }
+if (kb.presses('space')){
+bird.vel.y = -3;
+bird.sleeping = false;
+}
 
-//     if(kb.presses('s')){
-//         bird.vel.y=15
-//         bird.sleeping=false
-// }
-    
-    fill('blue');
-    textSize(14);
-    text('vel.y:'+bird.vel.y.toFixed(2),10,20);
-    text('isMoving:'+bird.isMoving,10,40);
-    text('sleeping:'+bird.sleeping,10,60);
+fill("blue");
+textSize(14);
+text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
+text('isMoving: ' + bird.isMoving,10,40);
+text('sleeping: ' + bird.sleeping,10,60);
+
+if (bird.vel.y < -1){
+bird.img = flapUpImg;
+bird.rotation = -30;
+}
+else if (bird.vel.y > 1){
+bird.img = flapDownImg;
+bird.rotation = 30;
+}
+else {
+bird.img = flapMidImg;
+bird.rotation = 0;
+}
+
+if (frameCount === 1){
+spawnPipePair();
+}
+}
+
+function spawnPipePair(){
+let gap = 50;
+let midY = height / 2;
+
+bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'static');
+bottomPipe.img = pipe;
+
+pipeGroup.add(bottomPipe);
+pipeGroup.layer = 0;
+
+topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, 'static');
+topPipe.img = pipe;
+topPipe.rotation = 180;
+
+pipeGroup.add(topPipe);
+
+
 }
