55697c1736da97c209d5cec86fb5b1321d47ba2f
wooof4 <tytus8392@gmail.com>
2026-08-23 04:18:47 +0000

    Aug 23, 2026, 4:18 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -10,7 +10,7 @@ flapDownImg = loadImage('assets/yellowbird-downflap.png');
 bg = loadImage('assets/background-night.png');
 base = loadImage('assets/base.png');
 pipe = loadImage('assets/pipe-green.png');
-gameoverImg=loadImage('a')
+gameoverImg=loadImage('assets/gameover.png');
 }
 
 function setup(){
