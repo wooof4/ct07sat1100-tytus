714dbc0eb93fc81dd6e63c0dacefbe1d9f3cf7cb
wooof4 <tytus8392@gmail.com>
2026-08-23 04:30:12 +0000

    Aug 23, 2026, 4:30 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -5,7 +5,7 @@ let gameoverImg;
 let gameoverLabel;
 let startScreenLabel;
 let startScreenImg;
-let start
+let startGame=false;
 function preload() {
 flapMidImg = loadImage('assets/yellowbird-midflap.png');
 flapUpImg = loadImage('assets/yellowbird-upflap.png');
