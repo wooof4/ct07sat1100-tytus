2abc1aacebf41d8605452a94fb799f5114daea91
wooof4 <tytus8392@gmail.com>
2026-08-30 03:35:43 +0000

    Aug 30, 2026, 3:35 AM

--- flappybird/flappybird04.js
+++ flappybird/flappybird04.js
@@ -6,9 +6,7 @@ let gameoverLabel;
 let startScreenLabel;
 let startScreenImg;
 let startGame=false;
-let score=0;
-let numberImages=[];
-let scoreDigits;
+
 function preload() {
 flapMidImg = loadImage('assets/yellowbird-midflap.png');
 flapUpImg = loadImage('assets/yellowbird-upflap.png');
--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -1,1 +1,4 @@
 // write your codes here
+let score=0;
+let numberImages=[];
+let scoreDigits;
