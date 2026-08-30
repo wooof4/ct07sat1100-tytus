9e37688c54fdfc13b040e2ef0474fb078aef9f6f
wooof4 <tytus8392@gmail.com>
2026-08-30 04:02:27 +0000

    Aug 30, 2026, 4:02 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -148,5 +148,6 @@ function drawScore(x,y,score,digitwidth,digitheight){
     let startX=x-totalwidth/2
     for (let i=0;i<scoreStr.length;i++){
         let digit =int(scoreStr[i]);
+        
     }
 }
