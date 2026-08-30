c3e42c128c2bad67b0451334abd3e0fc06bf0e5f
wooof4 <tytus8392@gmail.com>
2026-08-30 04:18:40 +0000

    Aug 30, 2026, 4:18 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -151,7 +151,7 @@ function drawScore(x,y,score,digitwidth,digitheight){
         let xPos=startX+i*digitwidth;
         let digitSprite=new scoreDigits.Sprite(xPos,y,digitwidth,digitheight);
         digitSprite.img=numberImages[digit];
-        moveGroup(scoreDigits)
+        moveGroup(scoreDigits,camer,x,24)
     }
 }
 function moveGroup(group,targetx,spacing){
