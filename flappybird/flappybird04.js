670588b1b2eb39c734e13ccde4a5ae97e52032a2
wooof4 <tytus8392@gmail.com>
2026-08-30 03:26:28 +0000

    Aug 30, 2026, 3:26 AM

--- flappybird/flappybird04.js
+++ flappybird/flappybird04.js
@@ -1,7 +1,11 @@
 let bird, floor;
 let flapMidImg, bg, base;
 let pipeGroup, pipe, bottomPipe, topPipe;
-
+let gameoverImg;
+let gameoverLabel;
+let startScreenLabel;
+let startScreenImg;
+let startGame=false;
 function preload() {
 flapMidImg = loadImage('assets/yellowbird-midflap.png');
 flapUpImg = loadImage('assets/yellowbird-upflap.png');
@@ -9,7 +13,8 @@ flapDownImg = loadImage('assets/yellowbird-downflap.png');
 bg = loadImage('assets/background-night.png');
 base = loadImage('assets/base.png');
 pipe = loadImage('assets/pipe-green.png');
-
+gameoverImg=loadImage('assets/gameover.png');
+startScreenImg=loadImage('assets/message.png')
 }
 
 function setup(){
@@ -23,7 +28,7 @@ bird.height = 30;
 bird.img = flapMidImg;
 
 bird.collider = "static";
-// bird.visibility=false;
+bird.visible=false
 bird.mass = 2;
 bird.drag = 0.02;
 bird.bounciness = 0.5;
@@ -38,16 +43,26 @@ floor.collider = "static";
 floor.img = base;
 
 pipeGroup = new Group();
+
+startScreenLabel=new Sprite(width/2,height/2,50,50,'none');
+startScreenLabel.img=startScreenImg;
 }
 
 function draw(){
 image(bg,0,0,width,height);
+if(kb.presses('space')||mouse.presses()){
+    startGame=true
+    startScreenLabel.visible=false
 
+}
+
+if(startGame){
 if (kb.presses('space')){
 bird.vel.y = -3;
 bird.sleeping = false;
 }
-
+bird.collider='dynamic';
+bird.visible=true;
 fill("blue");
 textSize(14);
 text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
@@ -70,23 +85,49 @@ bird.rotation = 0;
 if (frameCount === 1){
 spawnPipePair();
 }
+
+bird.x+=3;
+camera.x=bird.x;
+floor.x=bird.x;
+
+if(frameCount% 90 ===0){
+    spawnPipePair();
+}
+for(let pipe of pipeGroup){
+    if (pipe.x<-50){
+        pipe.remove();
+    }
+}
+
+if (bird.collides(pipeGroup)||bird.collides(floor)){
+    gameoverLabel=new Sprite(width/2,height/2,192,42);
+    gameoverLabel.img=gameoverImg;
+    gameoverLabel.layer=100;
+    gameoverLabel.x=camera.x;
+
+    noLoop();
+}
+}
+
+
 }
 
 function spawnPipePair(){
 let gap = 50;
-let midY = height / 2;
+let midY = random(250,height-250)
 
-bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'static');
+bottomPipe = new Sprite(bird.x+400, midY + gap / 2 + 200, 52, 320, 'static');
 bottomPipe.img = pipe;
 
 pipeGroup.add(bottomPipe);
 pipeGroup.layer = 0;
 
-topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, 'static');
+topPipe = new Sprite(bird.x+400, midY - gap / 2 - 200, 52, 320, 'static');
 topPipe.img = pipe;
 topPipe.rotation = 180;
 
 pipeGroup.add(topPipe);
+
 
 
 }
