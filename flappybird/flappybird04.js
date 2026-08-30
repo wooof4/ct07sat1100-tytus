f694bf0de2aca0a54df4510db53c3009120328b4
wooof4 <tytus8392@gmail.com>
2026-08-30 03:33:57 +0000

    Aug 30, 2026, 3:33 AM

--- flappybird/flappybird04.js
+++ flappybird/flappybird04.js
@@ -6,6 +6,8 @@ let gameoverLabel;
 let startScreenLabel;
 let startScreenImg;
 let startGame=false;
+let score=0;
+let n
 function preload() {
 flapMidImg = loadImage('assets/yellowbird-midflap.png');
 flapUpImg = loadImage('assets/yellowbird-upflap.png');
