515cde0390b9f76963460f37cf0b4c934bab9bb3
wooof4 <tytus8392@gmail.com>
2026-08-30 03:46:39 +0000

    Aug 30, 2026, 3:46 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -143,5 +143,5 @@ pipeGroup.add(topPipe);
 function drawScore(x,y,score,digitwidth,digitheight){
     scoreDigits.removeAll();
     let scoreStr=str(score);
-    let totalwidth=scoreStr.lengt
+    let totalwidth=scoreStr.length*digitwidth;
 }
