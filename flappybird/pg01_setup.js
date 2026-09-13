9ec31a63b02fee0305392db34ff7ab13cd7a47da
wooof4 <tytus8392@gmail.com>
2026-09-13 04:15:04 +0000

    Sep 13, 2026, 4:15 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -22,7 +22,7 @@ startScreenImg=loadImage('assets/message.png');
 for (let i = 0; i<10;i++){
     numberImages[i]=loadImage('assets/'+ i +'.png')
 }
-pointSound=createAudio('assets/sfx_wing.mp3')
+pointSound=createAudio('assets/sfx_point.mp3')
 }
 
 function setup(){
