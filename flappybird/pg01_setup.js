23d3977fdef09aee91aebd4db7c05b49b458330c
wooof4 <tytus8392@gmail.com>
2026-09-13 03:46:10 +0000

    Sep 13, 2026, 3:46 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -107,7 +107,7 @@ for(let pipe of pipeGroup){
         pipe.remove();
     }
 }
-for(let pipe)
+for(let pipe of pipeGroup)
 
 if (bird.collides(pipeGroup)||bird.collides(floor)){
     gameoverLabel=new Sprite(width/2,height/2,192,42);
