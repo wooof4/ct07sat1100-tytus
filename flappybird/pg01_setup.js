f30710dadeea67b5fc3281331ae61d4eea885a53
wooof4 <tytus8392@gmail.com>
2026-09-13 03:42:59 +0000

    Sep 13, 2026, 3:42 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -135,7 +135,7 @@ pipeGroup.layer = 0;
 topPipe = new Sprite(bird.x+400, midY - gap / 2 - 200, 52, 320, 'static');
 topPipe.img = pipe;
 topPipe.rotation = 180;
-top
+topPipe.passed= false;
 
 pipeGroup.add(topPipe);
 
