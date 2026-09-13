0a2736a7717c7220ce7aff50d80bddbdb9009deb
wooof4 <tytus8392@gmail.com>
2026-09-13 03:54:29 +0000

    Sep 13, 2026, 3:54 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -112,7 +112,7 @@ let pipeRightEdge =pipe.x +pipe.w/2;
 let birdLeftEdge = bird.x+bird.w/2;
 if (pipe.passed==false && pipeRightEdge<birdLeftEdge){
     pipe.passed=true
-    
+    score++;
 }
 }
 
