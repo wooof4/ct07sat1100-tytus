c441d2de593b02c05cbb53d0bf4cf2146e7261a1
wooof4 <tytus8392@gmail.com>
2026-09-20 04:25:41 +0000

    Sep 20, 2026, 4:25 AM

--- fruitninja/pg.js
+++ fruitninja/pg.js
@@ -5,11 +5,11 @@ function preload(){
 function setup(){
     createCanvas(800,600);
     
-    world.gravity.y=192.6
+    world.gravity.y=10
     i=new Sprite();
     i.x=400;
     i.y=300;
-    i.width=50;
+    i.width=75;
     i.height=75;
 }
 function draw(){
