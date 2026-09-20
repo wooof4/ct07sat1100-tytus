880da88a777b5d16ad34b70ce88daaba568c004c
wooof4 <tytus8392@gmail.com>
2026-09-20 03:13:34 +0000

    Sep 20, 2026, 3:13 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -1,12 +1,12 @@
 function setup(){
     createCanvas(600,400);
-    
-}
-function draw(){
     background('white');
     i=new Sprite();
     i.x=300;
     i.y=200;
     i.width=100;
-    i.height=100;
+    i.height=100;    
+}
+function draw(){
+
 }
