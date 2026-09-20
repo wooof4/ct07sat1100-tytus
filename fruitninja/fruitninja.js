b7506914f7ccf6f7614c7192fbb2c6da5677b00a
wooof4 <tytus8392@gmail.com>
2026-09-20 04:01:45 +0000

    Sep 20, 2026, 4:01 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -1,10 +1,10 @@
-let background;
+let backgrounds;
 function preload(){
     backgrounds=loadImage('assets/dojobackground.png')
 }
 function setup(){
     createCanvas(600,400);
-    background(backgrounds);
+    background();
     i=new Sprite();
     i.x=300;
     i.y=200;
